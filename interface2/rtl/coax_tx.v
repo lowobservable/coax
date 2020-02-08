0b618d6b3513cc1b88ba7c34f9a16b92cb84cc95
Andrew Kay <git@ajk.me>
2020-02-08 17:43:09 -0600

    Adding a 19 MHz clock

--- interface2/rtl/coax_tx.v
+++ interface2/rtl/coax_tx.v
@@ -4,12 +4,19 @@ module coax_tx (
     input clk,
     output tx
 );
-    reg state = 0;
+    parameter CLOCKS_PER_BIT = 8;
+
+    reg [$clog2(CLOCKS_PER_BIT):0] bit_counter = 0;
+
+    reg bit = 1'b1;
 
     always @(posedge clk)
     begin
-        state <= ~state;
+        if (bit_counter == CLOCKS_PER_BIT - 1)
+            bit_counter <= 0;
+        else
+            bit_counter <= bit_counter + 1;
     end
 
-    assign tx = state;
+    assign tx = bit_counter < (CLOCKS_PER_BIT / 2) ? ~bit : bit;
 endmodule
--- interface2/rtl/top.v
+++ interface2/rtl/top.v
@@ -5,8 +5,26 @@ module top (
     output tx,
     output usb_pu
 );
+    wire coax_clk;
+
+    // 19 MHz
+    //
+    // icepll -i 16 -o 18.869
+    SB_PLL40_CORE #(
+        .FEEDBACK_PATH("SIMPLE"),
+        .DIVR(4'b0000),        // DIVR =  0
+        .DIVF(7'b0100101),     // DIVF = 37
+        .DIVQ(3'b101),         // DIVQ =  5
+        .FILTER_RANGE(3'b001)  // FILTER_RANGE = 1
+    ) coax_clk_pll (
+        .RESETB(1'b1),
+        .BYPASS(1'b0),
+        .REFERENCECLK(clk),
+        .PLLOUTCORE(coax_clk)
+    );
+
     coax_tx coax_tx (
-        .clk(clk),
+        .clk(coax_clk),
         .tx(tx)
     );
 
