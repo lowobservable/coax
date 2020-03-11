0785407a49881a04b28e54cf8e865dadce44011e
Andrew Kay <git@ajk.me>
2020-03-10 22:37:34 -0500

    Add TX to top

--- interface2/rtl/top.v
+++ interface2/rtl/top.v
@@ -3,7 +3,15 @@
 module top (
     input clk_16mhz,
 
+    // Transmitter
+    input tx_load,
+    output tx_full,
+    output tx_active,
+    output tx_delay,
+    output tx_inverted,
+
     // Receiver
+    input rx_enable,
     input rx,
     output rx_active,
     output rx_data_available,
@@ -11,6 +19,8 @@ module top (
 
     // Shared data bus
     inout [9:0] data,
+
+    output debug,
 
     output usb_pu
 );
@@ -32,16 +42,38 @@ module top (
         .PLLOUTCORE(clk_19mhz)
     );
 
+    wire [9:0] tx_data;
+
+    assign tx_data = data;
+
+    coax_tx #(
+        .CLOCKS_PER_BIT(8)
+    ) coax_tx (
+        .clk(clk_19mhz),
+        .load(tx_load),
+        .data(tx_data),
+        .full(tx_full),
+        .active(tx_active),
+        .tx_delay(tx_delay),
+        .tx_inverted(tx_inverted)
+    );
+
+    wire [9:0] rx_data;
+
     coax_rx #(
         .CLOCKS_PER_BIT(8)
     ) coax_rx (
         .clk(clk_19mhz),
         .rx(rx),
         .active(rx_active),
-        .data(data),
+        .data(rx_data),
         .data_available(rx_data_available),
         .data_read(rx_data_read)
     );
+
+    assign data = rx_enable ? rx_data : 10'bzzzzzzzzzz;
+
+    assign debug = rx;
 
     assign usb_pu = 0;
 endmodule
