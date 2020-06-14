03af715ec5d3c058c10d00859fb3706074eccf16
Andrew Kay <git@ajk.me>
2020-06-14 10:05:02 -0500

    Start over

--- interface2/rtl/top.v
+++ interface2/rtl/top.v
@@ -3,25 +3,6 @@
 module top (
     input clk_16mhz,
 
-    // Transmitter
-    input tx_load,
-    output tx_full,
-    output tx_active,
-    output tx_delay,
-    output tx_inverted,
-
-    // Receiver
-    input rx_enable,
-    input rx,
-    output rx_active,
-    output rx_data_available,
-    input rx_data_read,
-
-    // Shared data bus
-    inout [9:0] data,
-
-    output debug,
-
     output usb_pu
 );
     // 19 MHz
@@ -41,40 +22,6 @@ module top (
         .REFERENCECLK(clk_16mhz),
         .PLLOUTCORE(clk_19mhz)
     );
-
-    wire [9:0] tx_data;
-
-    assign tx_data = data;
-
-    coax_tx #(
-        .CLOCKS_PER_BIT(8)
-    ) coax_tx (
-        .clk(clk_19mhz),
-        .load(tx_load),
-        .data(tx_data),
-        .full(tx_full),
-        .active(tx_active),
-        .tx_delay(tx_delay),
-        .tx_inverted(tx_inverted)
-    );
-
-    wire [9:0] rx_data;
-
-    coax_rx #(
-        .CLOCKS_PER_BIT(8)
-    ) coax_rx (
-        .clk(clk_19mhz),
-        .rx(rx),
-        .enable(rx_enable && !tx_active),
-        .active(rx_active),
-        .data(rx_data),
-        .data_available(rx_data_available),
-        .data_read(rx_data_read)
-    );
-
-    assign data = rx_enable ? rx_data : 10'bzzzzzzzzzz;
-
-    assign debug = rx;
 
     assign usb_pu = 0;
 endmodule
