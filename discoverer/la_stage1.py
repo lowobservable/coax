69276f177dcf933294ab5adf5fc1718acf964841
Andrew Kay <git@ajk.me>
2021-05-18 18:43:26 -0500

    something for the new CSVs

---
+++ discoverer/la_stage1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+
+def output_words(words):
+    print(words)
+
+def main():
+    with open(sys.argv[1], 'r') as file:
+        reader = csv.reader(file)
+
+        # Skip the header...
+        next(reader)
+
+        previous_data_available = None
+        previous_receiver_active = None
+
+        words = []
+
+        for line in reader:
+            data_available = bool(int(line[11]))
+            receiver_active = bool(int(line[12]))
+
+            if receiver_active and not previous_receiver_active:
+                if words:
+                    output_words(words)
+
+                words = []
+
+            if data_available and not previous_data_available:
+                bits = [int(column) for column in line[1:11]]
+
+                word = (bits[9] << 9) | (bits[8] << 8) | (bits[7] << 7) | (bits[6] << 6) | (bits[5] << 5) | (bits[4] << 4) | (bits[3] << 3) | (bits[2] << 2) | (bits[1] << 1) | bits[0]
+
+                words.append(word)
+
+            previous_data_available = data_available
+            previous_receiver_active = receiver_active
+
+        if words:
+            output_words(words)
+
+if __name__ == '__main__':
+    main()
---
+++ discoverer/la_stage2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys
+from enum import Enum
+
+from coax.protocol import Command, is_data_word, unpack_data_words
+
+from display import CHAR_MAP
+
+COMMANDS = { command.value: command.name for command in Command }
+
+# These got removed from the most recent pycoax...
+def is_command_word(word):
+    return (word & 0x1) == 1
+
+def unpack_command_word(word):
+    if not is_command_word(word):
+        raise ProtocolError(f'Word does not have command bit set: {word}')
+
+    command = (word >> 2) & 0x1f
+
+    return command
+
+def format_command(command):
+    if command in COMMANDS:
+        return '<' + COMMANDS[command] + '>'
+
+    return f'<{command:02x}>'
+
+def format_data(data, command):
+    return ' '.join(['{0:02x}'.format(byte) for byte in data])
+
+def main():
+    command = None
+    data = None
+
+    for line in sys.stdin:
+        line = line.strip()
+
+        if line.startswith('[') and line.endswith(']'):
+            words = [int(word) for word in line[1:-1].split(',')]
+
+            if is_command_word(words[0]) and all(is_data_word(word) for word in words[1:]):
+                command = unpack_command_word(words[0])
+                data = unpack_data_words(words[1:])
+
+                if command != Command.POLL.value:
+                    print('-> ' + format_command(command) + ' ' + format_data(data, command))
+            elif all(is_data_word(word) for word in words):
+                data = unpack_data_words(words)
+
+                if not (command == Command.POLL.value and len(words) == 1 and words[0] == 0):
+                    # Okay, perhaps the POLL was interesting so lets print it here.
+                    if command == Command.POLL.value:
+                        print('-> ' + format_command(command))
+
+
+                    print('?? ' + format_data(data, command))
+            else:
+                # Strange mix of commands and data...
+                pass
+
+if __name__ == '__main__':
+    main()
