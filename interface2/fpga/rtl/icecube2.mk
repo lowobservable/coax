61179295d4b0326d0d3b337f5ba0b7b5ac1c3715
Andrew Kay <git@ajk.me>
2021-10-03 10:27:41 -0500

    Update iCEcube2 makefile for CI

--- interface2/fpga/rtl/icecube2.mk
+++ interface2/fpga/rtl/icecube2.mk
@@ -1,20 +1,26 @@
 # See https://github.com/halfmanhalftaco/fpga-docker/tree/master/Lattice_iCEcube2
-ICECUBE2_WRAPPER = docker run -it --rm --volume $(RTL):/data --workdir /data --mac-address=$(shell cat .mac_address) icecube2:latest ./icecube2_env.sh
+ICECUBE2_WRAPPER = docker run -t --rm --volume $(RTL):/data --workdir /data --mac-address=$(shell cat .mac_address) icecube2:latest ./icecube2_env.sh
 
 RTL = $(dir $(realpath $(firstword $(MAKEFILE_LIST))))
 IMPLMNT = coax_Implmnt
 
-all: top.bin
+all: top.bin top_timing_report.txt
 
 $(IMPLMNT)/coax.edf: coax_syn.prj *.v third_party/*.v clocks.sdc
 	$(ICECUBE2_WRAPPER) synpwrap -prj coax_syn.prj
 
-top.bin: $(IMPLMNT)/coax.edf pins.pcf
-	rm -f top.bin
+$(IMPLMNT)/sbt/outputs/bitmap/top_bitmap.bin $(IMPLMNT)/sbt/outputs/router/top_timing.rpt: $(IMPLMNT)/coax.edf pins.pcf
 	$(ICECUBE2_WRAPPER) ./icecube2_flow.tcl
-	cp $(IMPLMNT)/sbt/outputs/bitmap/top_bitmap.bin top.bin
+
+top.bin: $(IMPLMNT)/sbt/outputs/bitmap/top_bitmap.bin
+	rm -f $@
+	cp $< $@
+
+top_timing_report.txt: $(IMPLMNT)/sbt/outputs/router/top_timing.rpt
+	rm -f $@
+	cp $< $@
 
 clean:
-	rm -rf $(IMPLMNT) synlog.tcl *.bin *.log *.log.bak
+	rm -rf $(IMPLMNT) synlog.tcl *.bin *_report.txt *.log *.log.bak
 
 .PHONY: all clean
